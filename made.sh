de015c7e4b2c68494733b4cdb9b53c3905c3aeca
Filly Agioro <50282743+FillyAgioro@users.noreply.github.com>
2020-11-27 12:35:24 +0530

    :zap: Added footer, Updated HTML files, and New colors

--- made.sh
+++ made.sh
@@ -3,7 +3,7 @@
 echo "Which website you redirect?"
 read website_url
 
-website_file=$(echo $website_url | cut -f 1 -d '.')
+title="MizLink"
 
 main() {
 	cat <<-_EOF_
@@ -12,7 +12,7 @@ main() {
 		<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>HTML Redirect Website</title>
+		<title>$title</title>
 		<link rel="icon" type="image/png" href="logo.png" />
 	_EOF_
 	return
@@ -26,7 +26,7 @@ redirect() {
 	_EOF_
 
 }
-echo $(redirect) >sites/$website_file.html
+echo $(redirect) >l/$website_url.html
 
 index() {
 	cat <<-_EOF_
@@ -41,25 +41,40 @@ index() {
 		color: #2c3e50;
 		}
 		a {
-		font-size: 1.5em;
+		font-size: 1.5rem;
 		background: #8e44ad;
 		color: #ecf0f1;
 		padding: .2em;
 		text-decoration: none;
+		line-height: 2em;
+		}
+		.footer {
+		background: #2c3e50;
+		padding: .2em;
+		color: #ecf0f1;
+		font-size: 1.5rem;
+		}
+		.footer a {
+		padding: 0em;
+		background: #2c3e50;
+		color: #ecf0f1;
 		}
 		</style>
 		</head>
 		<body>
-		<h1>HTML Redirect Website</h1>
+		<h1>$title</h1>
 	_EOF_
 
-	for i in sites/*.html; do
+	for i in l/*.html; do
 		cat <<-_EOF_
-			<a href="$i">$(echo "$website_file" | sed 's/.*/\u&/')</a>
+			<a href="$i">$(echo $i | cut -b 3-)</a>
 		_EOF_
 	done
 
 	cat <<-_EOF_
+		<p class="footer">@<a href="https://fillyagioro.vercel.app">Filly Agioro</a>
+		| <a href="https://github.com/FillyAgioro/mizlink">Website Source</a>
+		</p>
 		</body>
 		</html>
 	_EOF_
